2e339731de220fc390eb8cdb8f0a2e3a746d0149
Jeremy Clemans & Bonnie Bright <student@epicodus.com>
2014-04-28 16:06:12 -0700

    show complete todos

--- js/router.js
+++ js/router.js
@@ -2,6 +2,7 @@ Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function() {
     //additional routes here later
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -16,10 +17,22 @@ Todos.TodosIndexRoute = Ember.Route.extend({
     return this.modelFor('todos');
   }
 });
+
 Todos.TodosActiveRoute = Ember.Route.extend({
   model: function(){
     return this.store.filter('todo', function(todo) {
       return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
     });
   },
   renderTemplate: function(controller) {
